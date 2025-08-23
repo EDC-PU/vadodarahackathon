12fe612e0b4483b5a4a15eff8baf67a58536087e
Pranav Rathi <pranavrathi07@gmail.com>
2025-08-23 05:19:14 +0000

    WIP: update dashboard sidebar

--- src/components/dashboard-sidebar.tsx
+++ src/components/dashboard-sidebar.tsx
@@ -131,8 +131,8 @@ export default function DashboardSidebar() {
   return (
     <>
       <SidebarHeader className="border-b">
-        <div className="flex items-center justify-center gap-2 w-full p-2 h-20">
-          <Image src="https://www.pierc.org/vhlogo.png" alt="Vadodara Hackathon Logo" width={56} height={56} />
+        <div className="flex items-center justify-center gap-2 w-full p-2 h-28">
+          <Image src="https://www.pierc.org/vhlogo.png" alt="Vadodara Hackathon Logo" width={72} height={72} />
         </div>
       </SidebarHeader>
       <SidebarContent className="p-2">
